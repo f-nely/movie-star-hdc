6072b8e00dd9376e67e8c291a1e7c9b9b4fb6362
nely de faria <nelly.nf54@gmail.com>
2021-11-24 17:44:37 -0300

    Criando cabeçalho e rodapé

--- index.php
+++ index.php
@@ -19,8 +19,56 @@ require_once 'db.php';
   <title>MovieStar</title>
 </head>
 <body>
-  <h1>Testando MovieStar</h1>
-  <i class="far fa-plus-square"></i>
+  <header>
+    <nav id="main-navbar" class="navbar navbar-expand-lg">
+      <a href="<?= $BASE_URL ?>" class="navbar-brand">
+        <img src="img/logo.svg" id="logo" alt="MovieStar">
+        <span id="moviestar-title">MovieStar</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa bars"></i>
+      </button>
+      <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
+        <input type="text" name="q" id="search" class="form-control mr-s-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
+        <button class="btn my-2 my-sm-0" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </form>
+      <div class="collapse navbar-collapse" id="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="auth.php" class="nav-link">Entrar / Cadastrar</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <div id="main-container" class="container-fluid">
+    <h1>Corpo do site</h1>
+  </div>
+  <footer id="footer">
+    <div class="social-container">
+      <ul>
+        <li>
+          <a href="#"><i class="fab fa-facebook-square"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </li>
+      </ul>
+    </div>
+    <div id="footer-links-container">
+      <ul>
+        <li><a href="#">Adicionar filme</a></li>
+        <li><a href="#">Adicionar crítica</a></li>
+        <li><a href="#">Entrar / Registrar</a></li>
+      </ul>
+    </div>
+    <p>&copy; 2021 hdc</p>
+  </footer>
 
   <!-- BOOTSTRAP JS -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
