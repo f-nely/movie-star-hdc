a0f2835c37a7358d2c0fe7167fa8f5332aeb11d2
nely de faria <nelly.nf54@gmail.com>
2021-11-25 12:21:15 -0300

    Fazendo a separação em templates

--- index.php
+++ index.php
@@ -1,77 +1,12 @@
 <?php
-require_once 'globals.php';
-require_once 'db.php';
+require_once 'templates/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="short icon" href="img/moviestar.ico">
-  <!-- BOOTSTRAP -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/css/bootstrap.css" integrity="sha512-drnvWxqfgcU6sLzAJttJv7LKdjWn0nxWCSbEAtxJ/YYaZMyoNLovG7lPqZRdhgL1gAUfa+V7tbin8y+2llC1cw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- FONT AWESOME -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <!-- CSS do projeto -->
-  <link rel="stylesheet" href="css/styles.css">
-  <title>MovieStar</title>
-</head>
-<body>
-  <header>
-    <nav id="main-navbar" class="navbar navbar-expand-lg">
-      <a href="<?= $BASE_URL ?>" class="navbar-brand">
-        <img src="img/logo.svg" id="logo" alt="MovieStar">
-        <span id="moviestar-title">MovieStar</span>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
-        <i class="fas fa bars"></i>
-      </button>
-      <form action="" method="GET" id="search-form" class="form-inline my-2 my-lg-0">
-        <input type="text" name="q" id="search" class="form-control mr-s-2" type="search" placeholder="Buscar Filmes" aria-label="Search">
-        <button class="btn my-2 my-sm-0" type="submit">
-          <i class="fas fa-search"></i>
-        </button>
-      </form>
-      <div class="collapse navbar-collapse" id="navbar">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="auth.php" class="nav-link">Entrar / Cadastrar</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
-  <div id="main-container" class="container-fluid">
-    <h1>Corpo do site</h1>
-  </div>
-  <footer id="footer">
-    <div id="social-container">
-      <ul>
-        <li>
-          <a href="#"><i class="fab fa-facebook-square"></i></a>
-        </li>
-        <li>
-          <a href="#"><i class="fab fa-instagram"></i></a>
-        </li>
-        <li>
-          <a href="#"><i class="fab fa-youtube"></i></a>
-        </li>
-      </ul>
-    </div>
-    <div id="footer-links-container">
-      <ul>
-        <li><a href="#">Adicionar filme</a></li>
-        <li><a href="#">Adicionar crítica</a></li>
-        <li><a href="#">Entrar / Registrar</a></li>
-      </ul>
-    </div>
-    <p>&copy; 2021 hdc</p>
-  </footer>
+<div id="main-container" class="container-fluid">
+  <h1>Corpo do site</h1>
+</div>
 
-  <!-- BOOTSTRAP JS -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.5.3/js/bootstrap.js" integrity="sha512-KCgUnRzizZDFYoNEYmnqlo0PRE6rQkek9dE/oyIiCExStQ72O7GwIFfmPdkzk4OvZ/sbHKSLVeR4Gl3s7s679g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</body>
-</html>
+<?php
+require_once 'templates/footer.php';
+?>
+  
