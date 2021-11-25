ad70c493061bb05ecbf65b150215e31477da7fb6
nely de faria <nelly.nf54@gmail.com>
2021-11-25 16:49:30 -0300

    Criando estrutura do login

--- auth.php
+++ auth.php
@@ -3,7 +3,27 @@ require_once 'templates/header.php';
 ?>
 
 <div id="main-container" class="container-fluid">
-  <h1>Autenticação</h1>
+  <div class="col-md-12">
+    <div class="row" id="auth-row">
+      <div class="col-md-4" id="login-container">
+        <h2>Entrar</h2>
+        <form action="" method="POST">
+          <div class="form-group">
+            <label for="email">E-mail:</label>
+            <input type="text" class="form-control" name="email" id="email" placeholder="Digite seu e-mail">
+          </div>
+          <div class="form-group">
+            <label for="password">Senha:</label>
+            <input type="text" class="form-control" name="password" id="password" placeholder="Digite sua senha">
+          </div>
+          <input type="submit" class="btn card-btn" value="Entrar">
+        </form>
+      </div>
+      <div class="col-md-4" id="register-container">
+        <h2>Criar Conta</h2>
+      </div>
+    </div>
+  </div>
 </div>
 
 <?php
