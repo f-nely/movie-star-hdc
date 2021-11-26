d85fac6ececdb4f18b5e5b671cd0a4e979759a99
nely de faria <nelly.nf54@gmail.com>
2021-11-26 16:14:34 -0300

    Criando lógica do método authenticateUser

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -91,7 +91,29 @@ class UserDAO implements UserDAOInterface
 
   public function authenticateUser($email, $password)
   {
+    $user = $this->findByEmail($email);
 
+    if ($user) {
+      // checar se as senhas batem
+      if (password_verify($password, $user->password)) {
+        // gerar um token e inserir na session
+        $token = $user->generateToken();
+
+        $this->setTokenToSession($token);
+        // atualizar token no usuário
+        $user->token = $token;
+        
+        $this->update($user);
+
+        return true;
+
+      } else {
+        return false;
+      }
+
+    } else {
+      return false;
+    }
   }
 
   public function findByEmail($email)
