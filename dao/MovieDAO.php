89094092fba3005adcca4ffa4da9ab749ca0deb7
nely de faria <nelly.nf54@gmail.com>
2021-11-28 19:32:09 -0300

    Implementando método getLatestMovies

--- dao/MovieDAO.php
+++ dao/MovieDAO.php
@@ -39,7 +39,21 @@ class MovieDAO implements MovieDAOInterface
 
   public function getLatestMovies()
   {
+    $movies = [];
 
+    $stmt = $this->conn->query("SELECT * FROM movies ORDER BY id DESC");
+
+    $stmt->execute();
+
+    if ($stmt->rowCount() > 0) {
+      $moviesArray = $stmt->fetchAll();
+
+      foreach ($moviesArray as $movie) {
+        $movies[] = $this->buildMovie($movie);
+      }
+    } 
+    
+    return $movies;
   }
 
   public function getMoviesByCategory($category)
