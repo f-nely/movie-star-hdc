4278b9ab6ec86ec4a4daa722bb2927ffdbd5c204
nely de faria <nelly.nf54@gmail.com>
2021-11-29 17:13:10 -0300

    Criando dashboard

--- dashboard.php
+++ dashboard.php
@@ -13,3 +13,43 @@ $userDao = new UserDAO($conn, $BASE_URL);
 $userData = $userDao->verifyToken(true);
 ?>
 
+<div id="main-container" class="container-fluid">
+  <h2 class="section-title">Dashboard</h2>
+  <p class="section-description">Adicione ou atualize as informações dos filmes que você enviou</p>
+  <div class="cold-md-12" id="add-movie-container">
+    <a href="newmovie.php" class="btn card-btn">
+      <i class="fas fa-plus"></i> Adicionar filme
+    </a>
+  </div>
+  <div class="col-md-12" id="movies-dashboard">
+    <table class="table">
+      <thead>
+        <th scope="col">#</th>
+        <th scope="col">Título</th>
+        <th scope="col">Nota</th>
+        <th scope="col" class="actions-column">Ações</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td scope="row">1</td>
+          <td><a href="#" class="table-movie-title">Título</a></td>
+          <td><i class="fas fa-star"></i>9</td>
+          <td class="actions-column">
+            <a href="#" class="edit-btn">
+              <i class="far fa-edit"></i> Editar
+            </a>
+            <form action="">
+              <button type="submit" class="delete-btn">
+                <i class="fas fa-times"></i> Deletar
+              </button>
+            </form>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<?php
+  require_once 'templates/footer.php';
+?>
